b8e450c5571e24a1bc7ccb47af50e875f55905ff
Ryan Luong <lumintriet@gmail.com>
2026-09-16 09:11:34 +0700

    feat(types): define Result type.

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface Success<TData> {
+  success: true;
+  data: TData;
+}
+
+export interface Failure<TError extends Error> {
+  success: true;
+  error: TError;
+}
+
+export type Result<TData, TError extends Error> =
+  | Success<TData>
+  | Failure<TError>;
